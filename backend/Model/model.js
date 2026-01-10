373d5b37be63ee4bf4d965ad109ff0282b75d49b
Jian Lee <ramosjianlee@gmail.com>
2026-01-10 15:21:22 +0800

    feat: implement CRUD operations for todos in model.js

---
+++ backend/Model/model.js
@@ -0,0 +1,36 @@
+const pool = require("../Database/database");
+
+const getAllTodos = async () => {
+  const result = await pool.query("SELECT * FROM todos");
+  return result.rows;
+};
+
+const createTodo = async (description) => {
+  const result = await pool.query(
+    "INSERT INTO todos(description) VALUES($1) RETURNING *",
+    [description]
+  );
+  return result.rows[0];
+};
+
+const updateTodo = async (description, todo_id) => {
+  const result = await pool.query(
+    "UPDATE todos SET description = $1 WHERE todo_id=$2 RETURNING *",
+    [description, todo_id]
+  );
+  return result.rows[0];
+};
+
+const deleteTodo = async (todo_id) => {
+  const result = await pool.query(
+    "DELETE from todos WHERE todo = $1 RETURNING *"
+  );
+  return result.rows[0];
+};
+
+module.export = {
+  getAllTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+};
