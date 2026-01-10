68d5966f0ced9eb1f551232dcfb6b3915fb9f1cd
Jian Lee <ramosjianlee@gmail.com>
2026-01-10 15:21:27 +0800

    feat: implement CRUD operations for todos in controller.js

---
+++ backend/Controller/controller.js
@@ -0,0 +1,68 @@
+const todoModel = require("../Model/model");
+
+const getAllTodos = async (req, res) => {
+  try {
+    const todos = await todoModel.getAllTodos();
+    res.status(200).json({
+      message: "[Controller.js]: Fetching all todos successful!",
+      data: todos,
+    });
+  } catch (err) {
+    console.error("[Controller.js]: Error fetching all todos!", err.message);
+    res.status(500).json({ error: "Server error!" });
+  }
+};
+
+const createTodo = async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await todoModel.createTodos(description);
+    res.status(200).json({
+      message: "[Controller.js]: Creating new todo succcesful!",
+      data: newTodo,
+    });
+  } catch (err) {
+    console.error("[Controller.js]: Error adding new todo!", err.message);
+    res.status(500).json({ error: "Server error!" });
+  }
+};
+
+const updateTodo = async (req, res) => {
+  try {
+    const { todo_id } = req.params;
+    const { description } = req.body;
+    const updatedTodo = await todoModel.updateTodo(description, todo_id);
+
+    if (!todo_id && !description) {
+      res.status(400).json({ message: "[Controller.js]: To do not found..." });
+    }
+
+    res.status(200).json({
+      message: "[Controller.js]: Updating todo successful!",
+      data: updatedTodo,
+    });
+  } catch (err) {
+    console.error("[Controller.js]: Error updating to do!", err.message);
+    res.status(500).json({ error: "Server error!" });
+  }
+};
+
+const deleteTodo = async (req, res) => {
+  try {
+    const { todo_id } = req.params;
+    await todoModel.deleteTodo(todo_id);
+    return res
+      .status(200)
+      .json({ message: "[Controller.js]: Deleting to do successful!" });
+  } catch (err) {
+    console.error("[Controller.js]: Error deleting to do!", err.message);
+    res.status(500).json({ error: "Server error!" });
+  }
+};
+
+module.exports = {
+  getAllTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+};
