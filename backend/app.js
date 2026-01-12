c8fedb9bed42aa7a0dbf039c15020c8469038049
Jian Lee <ramosjianlee@gmail.com>
2026-01-12 21:11:02 +0800

    feat: update route path for todos to /warmuptodos in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-const todoRoutes = require("../backend/Routes/routes");
+const router = require("../backend/Routes/routes.js");
 const express = require("express");
 const cors = require("cors");
 
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use("/todos", todoRoutes);
+app.use("/warmuptodos", router);
 
 module.exports = app;
