4a98d34496ff718445d9998b99f18b6e8b4e7682
Jian Lee <ramosjianlee@gmail.com>
2026-01-12 21:12:22 +0800

    hard reset xD

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const app = require("./app.js");
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server now listening to http://localhost:${PORT}`);
-});
