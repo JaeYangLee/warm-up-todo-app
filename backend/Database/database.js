13e4edaa429f95b89db1c06181480875f6066918
Jian Lee <ramosjianlee@gmail.com>
2026-02-02 22:07:16 +0800

    feat: add database connection setup using pg and dotenv

---
+++ backend/Database/database.js
@@ -0,0 +1,15 @@
+const pkg = require("pg");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+module.exports = pool;
